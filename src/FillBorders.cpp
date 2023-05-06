e976baf053c7d62d94762c186fc5d2b47f43f823
DTL2020 <68707763+DTL2020@users.noreply.github.com>
2023-05-06 14:27:54 +0300

    Added left border filtering for mode=4

--- src/FillBorders.cpp
+++ src/FillBorders.cpp
@@ -3,7 +3,10 @@
 #include <cmath>
 #include <memory>
 
-#include "avisynth.h"
+#include "include\avisynth.h"
+
+#define TS_KERNELSIZE 5 // odd number
+#define MAX_TSIZE 10 // even number - maximum supported total size of transient
 
 template <typename T>
 AVS_FORCEINLINE void memset16(T* ptr, T value, size_t num)
@@ -35,6 +38,8 @@ class FillBorders : public GenericVideoFilter
     int m_mode;
     std::array<int, 4> process;
     bool has_at_least_v8;
+	int trsize;
+	float ts_kernel[TS_KERNELSIZE];
 
     template<typename T, typename T1>
     PVideoFrame fill(PVideoFrame frame, IScriptEnvironment* env);
@@ -42,7 +47,7 @@ class FillBorders : public GenericVideoFilter
     PVideoFrame(FillBorders::* processing)(PVideoFrame frame, IScriptEnvironment* env);
 
 public:
-    FillBorders(PClip _child, AVSValue left, AVSValue top, AVSValue right, AVSValue bottom, int mode, int y, int u, int v, int a, IScriptEnvironment* env);
+    FillBorders(PClip _child, AVSValue left, AVSValue top, AVSValue right, AVSValue bottom, int mode, int y, int u, int v, int a, int ts, IScriptEnvironment* env);
     PVideoFrame __stdcall GetFrame(int n, IScriptEnvironment* env);
     int __stdcall SetCacheHints(int cachehints, int frame_range)
     {
@@ -57,6 +62,8 @@ PVideoFrame FillBorders::fill(PVideoFrame frame, IScriptEnvironment* env)
     constexpr std::array<int, 4> planes_y{ PLANAR_Y, PLANAR_U, PLANAR_V, PLANAR_A };
     constexpr std::array<int, 4> planes_r{ PLANAR_G, PLANAR_B, PLANAR_R, PLANAR_A };
     const int* planes{ vi.IsRGB() ? planes_r.data() : planes_y.data() };
+
+	T temp_buf[MAX_TSIZE];
 
     for (int i{ 0 }; i < vi.NumComponents(); ++i)
     {
@@ -201,6 +208,65 @@ PVideoFrame FillBorders::fill(PVideoFrame frame, IScriptEnvironment* env)
 
                     for (int y{ 0 }; y < m_bottom[i]; ++y)
                         memcpy(dstp + stride * (static_cast<int64_t>(height) - m_bottom[i] + static_cast<int64_t>(y)), dstp + static_cast<int64_t>(stride) * (m_top[i] + static_cast<int64_t>(y)), static_cast<int64_t>(stride) * sizeof(T));
+
+
+					if (trsize > 0)
+					{
+						if (m_left[i] > 0)
+						{
+							for (int y{ m_top[i] }; y < height - m_bottom[i]; ++y)
+							{
+								T* srcp;
+								T sample0;
+								bool bleftspecial = false;
+								if ((m_left[i] - (trsize + TS_KERNELSIZE / 2)) > 0)
+									srcp = dstp + static_cast<int64_t>(stride)* y + static_cast<int64_t>(m_left[i] - trsize);
+								else
+								{
+									srcp = dstp + static_cast<int64_t>(stride)* y;
+									sample0 = srcp[0];
+									bleftspecial = true;
+								}
+
+								for (int xp{ 0 }; xp < trsize * 2; ++xp)
+								{
+									float sum = 0.0f;
+									for (int k{ 0 }; k < TS_KERNELSIZE; ++k)
+									{
+										if (!bleftspecial)
+											sum += (float)srcp[xp + k - TS_KERNELSIZE / 2] * ts_kernel[k];
+										else
+										{
+											T sample;
+											if (xp + k + m_left[i] - trsize - (TS_KERNELSIZE / 2) < 0)
+												sample = sample0;
+											else
+												sample = srcp[xp + k + m_left[i] - trsize - TS_KERNELSIZE / 2];
+
+											sum += (float)sample * ts_kernel[k];
+										}
+									}
+									if (sizeof(T) < 4)
+										temp_buf[xp] = T(sum + 0.5f);
+									else
+										temp_buf[xp] = sum;
+								}
+
+								//copy back processed buf
+								if (!bleftspecial)
+									memcpy(srcp, temp_buf, static_cast<int64_t>(trsize * 2) * sizeof(T));
+								else
+									memcpy(srcp + static_cast<int64_t>(m_left[i] - trsize), temp_buf, static_cast<int64_t>(trsize * 2) * sizeof(T));
+
+								//						for (int x{ 0 }; x < m_left[i]; ++x)
+								//							dstp[stride * y + x] = dstp[stride * y + width - m_right[i] - m_left[i] + x];
+
+							}
+
+						}
+
+					}
+
                     break;
                 case 5:
                 {
@@ -469,7 +535,7 @@ PVideoFrame FillBorders::fill(PVideoFrame frame, IScriptEnvironment* env)
     return frame;
 }
 
-FillBorders::FillBorders(PClip _child, AVSValue left, AVSValue top, AVSValue right, AVSValue bottom, int mode, int y, int u, int v, int a, IScriptEnvironment* env)
+FillBorders::FillBorders(PClip _child, AVSValue left, AVSValue top, AVSValue right, AVSValue bottom, int mode, int y, int u, int v, int a, int ts, IScriptEnvironment* env)
     : GenericVideoFilter(_child), m_mode(mode), process{ 1, 1, 1, 1 }
 {
     if (!vi.IsPlanar())
@@ -482,8 +548,13 @@ FillBorders::FillBorders(PClip _child, AVSValue left, AVSValue top, AVSValue rig
     if (num_left > vi.NumComponents())
         env->ThrowError("FillBorders: more left given than there are planes");
 
-    for (int i{ 0 }; i < num_left; ++i)
-        m_left[i] = left[i].AsInt();
+	for (int i{ 0 }; i < num_left; ++i)
+	{
+		m_left[i] = left[i].AsInt();
+		if (ts > m_left[i]) env->ThrowError("FillBorders: ts must be lower or equal to fill size");
+	}
+
+	trsize = ts;
 
     switch (num_left)
     {
@@ -634,10 +705,40 @@ FillBorders::FillBorders(PClip _child, AVSValue left, AVSValue top, AVSValue rig
 
     switch (vi.ComponentSize())
     {
-        case 1: processing = &FillBorders::fill<uint8_t, int>; break;
-        case 2: processing = &FillBorders::fill<uint16_t, int>; break;
-        default: processing = &FillBorders::fill<float, float>; break;
+        case 1: 
+			processing = &FillBorders::fill<uint8_t, int>; 
+			break;
+        case 2: 
+			processing = &FillBorders::fill<uint16_t, int>;
+			break;
+        default: 
+			processing = &FillBorders::fill<float, float>; 
+			break;
     }
+
+	if (trsize != 0)
+	{
+		// make simple gauss kernel at first
+		float p = 1.2f; // make it ts_kernel param (1)
+		for (int i = 0; i < TS_KERNELSIZE; i++)
+		{
+			int val = i - TS_KERNELSIZE / 2;
+			ts_kernel[i] = (float)pow(2.0, -p * val * val);
+		}
+
+		// normalize
+		float sum = 0;
+		for (int i = 0; i < TS_KERNELSIZE; i++)
+		{
+			sum += ts_kernel[i];
+		}
+
+		for (int i = 0; i < TS_KERNELSIZE; i++)
+		{
+			ts_kernel[i] /= sum;
+		}
+
+	}
 }
 
 PVideoFrame __stdcall FillBorders::GetFrame(int n, IScriptEnvironment* env)
@@ -651,12 +752,12 @@ PVideoFrame __stdcall FillBorders::GetFrame(int n, IScriptEnvironment* env)
             env->ThrowError("FillBorders: frame must be not interlaced.");
     }
 
-    return (this->*processing)(frame, env);
+	return (this->*processing)(frame, env);
 }
 
 AVSValue __cdecl Create_FillBorders(AVSValue args, void* user_data, IScriptEnvironment* env)
 {
-    enum { Clip, Left, Top, Right, Bottom, Mode, Y, U, V, A };
+    enum { Clip, Left, Top, Right, Bottom, Mode, Y, U, V, A, ts };
 
     return new FillBorders(
         args[Clip].AsClip(),
@@ -669,13 +770,14 @@ AVSValue __cdecl Create_FillBorders(AVSValue args, void* user_data, IScriptEnvir
         args[U].AsInt(3),
         args[V].AsInt(3),
         args[A].AsInt(3),
+		args[ts].AsInt(3),
         env);
 }
 
 class Arguments
 {
-    AVSValue _args[9];
-    const char* _arg_names[9];
+    AVSValue _args[10];
+    const char* _arg_names[10];
     int _idx;
 
 public:
@@ -729,7 +831,7 @@ const char* __stdcall AvisynthPluginInit3(IScriptEnvironment * env, const AVS_Li
 {
     AVS_linkage = vectors;
 
-    env->AddFunction("FillBorders", "c[left]i*[top]i*[right]i*[bottom]i*[mode]i[y]i[u]i[v]i[a]i", Create_FillBorders, 0);
+    env->AddFunction("FillBorders", "c[left]i*[top]i*[right]i*[bottom]i*[mode]i[y]i[u]i[v]i[a]i[ts]i", Create_FillBorders, 0);
     env->AddFunction("FillMargins", "c[left]i[top]i[right]i[bottom]i[y]i[u]i[v]i", Create_FillMargins, 0);
     return "FillBorders";
 }
